3964bd2b7a4881df962844750bc21ffa265d484d
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-07-18 03:39:54 +0000

    Feat(run):Add welcome message and selec menu

--- run.py
+++ run.py
@@ -3,4 +3,52 @@ import sys
 from termcolor import colored, cprint
 
 
+def opening_function():
 
+    """
+    This is the welcome message and the instructions
+    """
+    print(colored(
+        "Welcome to Random Madlibs.\n" +
+        "Please select the theme of your madlibs:\n" +
+        "Type 1 for Instructions for the Babysiter.\n"
+        "Type 2 for Going to the doctor.\n"
+        "Type 3 for Bats are so cool.\n",
+        "yellow", attrs=["bold"]))
+    main_choice()
+
+
+def madlibs01():
+    print("MADLIBS 1 HERE")
+
+def madlibs01():
+    print("MADLIBS 2 HERE")
+
+def madlibs01():
+    print("MADLIBS 3 HERE")
+
+
+def main_choice():
+    selection = int(input(colored(
+        "select Madlibs theme: ", "blue", attrs=["reverse", "blink"]
+        )))
+
+    """
+    This function allows the user to select a theme
+    """
+    if selection == 1:
+        madlibs01()
+    elif selection == 2:
+        madlibs02()
+    elif selection == 3:
+        madlibs03()
+    else:
+        print("Invalid selection, please try again")
+        main_choice()
+
+
+def main():
+    opening_function()
+
+
+main()
